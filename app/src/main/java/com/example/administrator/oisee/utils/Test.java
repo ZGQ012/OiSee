9ba73565922ef1ba5e37ccaeba531cf205b1e132
2380253499@qq.com <2380253499@qq.com>
2018-10-13 16:25:49 +0800

    测试

--- app/src/main/java/com/example/administrator/oisee/utils/Test.java
+++ app/src/main/java/com/example/administrator/oisee/utils/Test.java
@@ -3,10 +3,9 @@ package com.example.administrator.oisee.utils;
 import android.util.Log;
 
 public class Test {
-
     private static final String TAG = "Test";
 
-    public void test2(){
-        Log.i(TAG, "test2: ");
+    public void test1() {
+        Log.i(TAG, "test123: ");
     }
 }
