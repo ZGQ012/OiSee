2c61f2a7a399c27246c21cef24ad568875db76a9
Your Name <you@example.com>
2018-10-13 16:23:17 +0800

    测试

--- app/src/main/java/com/example/administrator/oisee/utils/Test.java
+++ app/src/main/java/com/example/administrator/oisee/utils/Test.java
@@ -1,4 +1,12 @@
 package com.example.administrator.oisee.utils;
 
+import android.util.Log;
+
 public class Test {
+
+    private static final String TAG = "Test";
+
+    public void test2(){
+        Log.i(TAG, "test2: ");
+    }
 }
